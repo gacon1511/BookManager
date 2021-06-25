3f8487814776525e150289dd3d34d80e5880b67d
gacon1511 <hoaiphuc15987@gmail.com>
2021-06-25 20:20:53 +0700

    Initial commit

---
+++ BookManager/Controllers/BookController.cs
@@ -0,0 +1,99 @@
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookManager.Controllers
+{
+    public class BookController : Controller
+    {
+        // GET: Book
+        public ActionResult ListBook()
+        {
+            BookManagerContext context = new BookManagerContext();
+            var listBook = context.Books.ToList();
+            return View(listBook);
+        }
+
+        [Authorize]
+        public ActionResult Buy(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Book book)
+        {
+            if(ModelState.IsValid)
+            {
+                BookManagerContext context = new BookManagerContext();
+                context.Books.AddOrUpdate(book);
+                context.SaveChanges();
+                return RedirectToAction("ListBook");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+        [Authorize]
+        [HttpPost]
+        public ActionResult Edit(Book book)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book bookUpdate = context.Books.SingleOrDefault(p => p.ID == book.ID);
+            if(bookUpdate != null)
+            {
+                context.Books.AddOrUpdate(book);
+                context.SaveChanges();
+            }
+            return RedirectToAction("ListBook");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if(book == null)
+            {
+                return HttpNotFound();
+            }
+            return View(book);
+        }
+
+        [Authorize]
+       
+        public ActionResult DeleteBook(int id)
+        {
+            BookManagerContext context = new BookManagerContext();
+            Book book = context.Books.SingleOrDefault(p => p.ID == id);
+            if(book != null)
+            {
+                context.Books.Remove(book);
+                context.SaveChanges();
+            }
+            return RedirectToAction("ListBook");
+        }
+    }
+}
---
+++ BookManager/Models/Book.cs
@@ -0,0 +1,34 @@
+﻿namespace BookManager.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Book")]
+    public partial class Book
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Tiêu đề không được để trống")]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Nội dung không được để trống")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Tác giả không được để trống")]
+        [StringLength(255)]
+        public string Author { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Images { get; set; }
+        [Required]
+        [Range(1000,1000000, ErrorMessage = "Giá tiền nằm trong khoảng 1k đến 1m")]
+        public int Price { get; set; }
+    }
+}
